872bea3a0048dc7c16568c5e41919e11f7e43ef2
viphavethcm <loivang.nhatduy@gmail.com>
2022-03-25 01:41:29 +0700

    Kadane_Algorithms

--- src/algorithms/Kadane_Algorithms.java
+++ src/algorithms/Kadane_Algorithms.java
@@ -1,11 +1,11 @@
 package algorithms;
 
 public class Kadane_Algorithms {
-    public static void main(String[] args) {
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-
-        System.out.println(maxSubArray(nums));
-    }
+//    public static void main(String[] args) {
+//        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+//
+//        System.out.println(maxSubArray(nums));
+//    }
 
     public static int maxSubArray(int[] nums) {
         int cur = nums[0];
